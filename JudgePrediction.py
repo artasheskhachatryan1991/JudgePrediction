55d77eb7504fc70ed06a54e87dce4d501e9249e2
artashes.khachatryan <artashes.khachatryan@synisys.com>
2018-08-06 16:29:43 +0400

    commit

--- JudgePrediction.py
+++ JudgePrediction.py
@@ -20,30 +20,31 @@ import pyodbc
 from sklearn.externals import joblib
 TABLE_LIST = ["CourtCase", "CourtCaseParty", "CourtCaseSchedule", "CourtCaseCrimes", "CourtCasePartyLegalRepresentative", "CourtCaseDocument"]
 
-def load_data(conn, table, is_train = True, train_decision_date = None, test_id = None):    
-    cursor = conn.cursor()       
-    sql = 'select * from dbo.load_' + table    
-    if table == 'CourtCase':
-        sql += '(?, ?)'
-        cursor.execute(sql, (test_id, train_decision_date))
-        rows = np.array(cursor.fetchall())
-        columns = np.array(cursor.description)[:, 0]            
-        assert len(rows) > 0, 'No data found for specified filters'
-    else:
-        sql += '(?)'
-        cursor.execute(sql, (test_id))
-        rows = np.array(cursor.fetchall())
-        columns = np.array(cursor.description)[:, 0]
-    return pd.DataFrame(data=list(rows), columns=columns)
+def load_data(conn, table, is_train = True, test_id = None, load_from_disk = False):
+	if is_train:
+		if load_from_disk:
+			return pd.read_pickle(table + '.pkl')	
+	cursor = conn.cursor()       
+	sql = 'select * from dbo.load_' + table + '(?)'
+	print(sql, '----', test_id)
+	cursor.execute(sql, (test_id))
+	columns = np.array(cursor.description)[:, 0]
+	rows = np.array(cursor.fetchall())	
+	if table == 'CourtCase':
+		assert len(rows) > 0, 'No data found for specified filters'
+	data = pd.DataFrame(data=list(rows), columns=columns)
+	if is_train:
+		data.to_pickle(table + ".pkl")
+	return data
 
-def load_all_data(conn, tables, is_train = True, train_decision_date = None, test_id = None):
+def load_all_data(conn, tables, is_train = True, test_id = None, load_from_disk = False):
     try:
         assert is_train or test_id != None
     except AssertionError():
         raise AssertionError("test_id must be passed in training mode") 
     results = {}    
     for table in tables:
-        results[table] = load_data(conn, table, is_train, train_decision_date, test_id)
+        results[table] = load_data(conn, table, is_train, test_id, load_from_disk = load_from_disk)
         print(f'{table} data has been processed')            
     return results
 
@@ -57,8 +58,8 @@ def data_preprocessing(data):
     
     CourtCase = CourtCase.sample(frac=1)
 
-    CourtCaseParty["IsNPPA"] = CourtCaseParty.PartyID.apply(lambda x: 1 if x == -2 else 0)
-
+    CourtCaseParty["IsNPPA"] = CourtCaseParty.PartyInstanceID.apply(lambda x: 1 if x == -2 else 0)
+    
     ccp_aggregations = {}
     ccp_aggregations["CourtCasePartyID"] = {"ccp_count": "count"}
     ccp_aggregations["IsNPPA"] = {"is_nppa_present": "max"}
@@ -77,9 +78,7 @@ def data_preprocessing(data):
     CourtCaseCrimes = CourtCaseCrimes.groupby("CourtCaseID").count()
     CourtCase = pd.merge(CourtCase, CourtCaseCrimes, how='left', left_on="CourtCaseID", right_index=True)
 
-    CourtCase = pd.merge(CourtCase, CourtCasePartyLegalRepresentative, how='left', left_on="CourtCaseID", right_on="CourtCaseID")
-
-
+    CourtCase = pd.merge(CourtCase, CourtCasePartyLegalRepresentative, how='left', left_on="CourtCaseID", right_on="CourtCaseID")    
     # CourtCaseAddmisibility = pd.read_csv("CourtCaseAddmisibility.csv")
     # ccai_aggregations = {"AdmissibilityItemID" : {"cnt_admitem" : "count"}}
     # CourtCaseAddmisibility = CourtCaseAddmisibility.groupby("CourtCaseID").agg({**ccai_aggregations})
@@ -125,7 +124,7 @@ def data_preprocessing(data):
     CourtCase["GenderBasedViolence"] = CourtCase.GenderBasedViolence.fillna(0)
     CourtCase["InitiatedFromAbunzi"] = CourtCase.InitiatedFromAbunzi.fillna(0)
     CourtCase["SolvedFromAbunzi"] = CourtCase.SolvedFromAbunzi.fillna(0)
-    CourtCase["HasDetails"] = CourtCase.HasDetails.fillna(0)
+    #CourtCase["HasDetails"] = CourtCase.HasDetails.fillna(0)
     CourtCase["IsExempted"] = CourtCase.IsExempted.fillna(0)
     CourtCase["AttachedDate"] = CourtCase.AttachedDate.fillna(0)
     CourtCase.drop(columns=["HasPassedCaseNumberAllocated", "CaseCode", "MinorVersion", "MajorVersion", "CourtID", "CourtCaseID"
@@ -139,7 +138,9 @@ def data_preprocessing(data):
        'CaseRejectionID', 'ExtraOrdinaryProcedureID', 'PreviousCourtCaseID',
        'SpecialCaseID']
     CourtCase = CourtCase.drop(columns=drop_col)
+    #print(CourtCase.isna().any())
     CourtCase.dropna(inplace=True)
+    print(CourtCase.shape)
     
     assert len(CourtCase) > 0, 'No data left after preprocessing'    
 
@@ -160,25 +161,18 @@ def get_connection(path):
     return conn
 
 def parse_args_train(*argument_array):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--properties_path',
-                        default = '\db_properties\db_connection.json',
-                        help='relative path to database connection properties')
-    parser.add_argument('--train_decision_date',                        
-                        help='date until where to train the data')
-    parser.add_argument('--load_model', '-l',
-                        help='load model from disk')
-    args = parser.parse_args(*argument_array)
-    return args
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--properties_path'
+						,default = '\db_properties\db_connection.json'
+						,help='relative path to database connection properties')
+	parser.add_argument('--load_model', '-l'
+						,help='load model from disk')
+	parser.add_argument('--load_from_disk', '-ld'
+						, help='load data from disk')
+	args = parser.parse_args(*argument_array)
+	return args
 
-def parse_args_test(*argument_array):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--test_id',                        
-                        help='case id to test the duration', )    
-    args = parser.parse_args(*argument_array)
-    return args
-
-def init(PROPERTIES_PATH, TRAIN_DECISION_DATE):
+def init(PROPERTIES_PATH, LOAD_FROM_DISK):
 	
 	
 	boost_params = {'n_estimators': 200,
@@ -190,7 +184,7 @@ def init(PROPERTIES_PATH, TRAIN_DECISION_DATE):
  
 	boost = GradientBoostingRegressor(**boost_params)
 
-	train_data = load_all_data(get_connection(PROPERTIES_PATH), TABLE_LIST, is_train=True, train_decision_date=TRAIN_DECISION_DATE)
+	train_data = load_all_data(get_connection(PROPERTIES_PATH), TABLE_LIST, is_train=True, load_from_disk = LOAD_FROM_DISK)
 	train_data = data_preprocessing(train_data)
 	train_X, train_Y = train_data
 	boost.fit(train_X, train_Y)
@@ -211,7 +205,7 @@ def predict(boost, caseId, judgeId, properties_path):
 	#judge_id = input("enter JudgeID to predict the duration ")
 	judge_id = judgeId # int(judge_id)
 	
-	test_data = load_all_data(get_connection(properties_path), TABLE_LIST, is_train=False, train_decision_date=None, test_id=TEST_ID)
+	test_data = load_all_data(get_connection(properties_path), TABLE_LIST, is_train=False, test_id=TEST_ID)
 	test_data = data_preprocessing(test_data)
 	test_X, test_Y = test_data
 	
@@ -232,13 +226,12 @@ def predict(boost, caseId, judgeId, properties_path):
 
 def main(args):
 	properties_path = args.properties_path
+	laod_from_disk = args.load_from_disk
 	
 	if args.load_model:
 		boost = joblib.load('model.pkl')
-	else:
-		TRAIN_DECISION_DATE = args.train_decision_date
-
-		boost = init(properties_path, TRAIN_DECISION_DATE)
+	else:		
+		boost = init(properties_path, laod_from_disk)
 		joblib.dump(boost, 'model.pkl')
 	
 	while True:

cd46fed9cc464f262c966d2c0eac4b6aefcabe42
artashes.khachatryan <artashes.khachatryan@synisys.com>
2018-08-14 11:44:01 +0400

    commit

--- JudgePrediction.py
+++ JudgePrediction.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 import argparse
 from scipy.stats import randint as sp_randint
-from sklearn.metrics import accuracy_score, r2_score
-from sklearn.model_selection import cross_val_score, RandomizedSearchCV
+from sklearn.metrics import accuracy_score, r2_score, mean_squared_log_error
+from sklearn.model_selection import cross_val_score, RandomizedSearchCV, train_test_split
 from sklearn.preprocessing import PolynomialFeatures, LabelEncoder
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, BayesianRidge
 from sklearn.svm import SVR
@@ -15,9 +15,14 @@ from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientB
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
+from xgboost.sklearn import XGBRegressor
 import xgboost as xgb
+from xgboost import plot_importance
 import pyodbc
+from lightgbm.sklearn import LGBMRegressor
+import lightgbm as lgb
 from sklearn.externals import joblib
+from sklearn.model_selection import GridSearchCV
 TABLE_LIST = ["CourtCase", "CourtCaseParty", "CourtCaseSchedule", "CourtCaseCrimes", "CourtCasePartyLegalRepresentative", "CourtCaseDocument"]
 
 def load_data(conn, table, is_train = True, test_id = None, load_from_disk = False):
@@ -25,7 +30,7 @@ def load_data(conn, table, is_train = True, test_id = None, load_from_disk = Fal
 		if load_from_disk:
 			return pd.read_pickle(table + '.pkl')	
 	cursor = conn.cursor()       
-	sql = 'select * from dbo.load_' + table + '(?)'
+	sql = 'exec dbo.load_' + table + ' @CourtCaseID = ?'
 	print(sql, '----', test_id)
 	cursor.execute(sql, (test_id))
 	columns = np.array(cursor.description)[:, 0]
@@ -36,7 +41,7 @@ def load_data(conn, table, is_train = True, test_id = None, load_from_disk = Fal
 	if is_train:
 		data.to_pickle(table + ".pkl")
 	return data
-
+	
 def load_all_data(conn, tables, is_train = True, test_id = None, load_from_disk = False):
     try:
         assert is_train or test_id != None
@@ -105,6 +110,7 @@ def data_preprocessing(data):
     CourtCase = pd.merge(CourtCase, CourtCaseDocument, how='left', left_on="CourtCaseID", right_index=True)
 
     [CourtCase[x].fillna(0, inplace=True) for x in CourtCase.columns if x.startswith("ArticleID")];
+    [CourtCase[x].fillna(0, inplace=True) for x in CourtCase.columns if x.startswith("DocumentTypeID")];
     CourtCase.CountOfLegalRepresentative.fillna(0, inplace=True)
     CourtCase.ccp_count.fillna(0, inplace=True)
     # CourtCase.cnt_admitem.fillna(0, inplace=True)
@@ -112,10 +118,11 @@ def data_preprocessing(data):
     CourtCase.is_nppa_present.fillna(0, inplace=True)        
 
     CourtCase["HasRecieptDocument"] = CourtCase.ReceiptDocumentID.fillna(0).apply(lambda x: 1 if x > 0 else 0)
-    CourtCase["HasProsecutionCase"] = CourtCase.ProsecutionCaseID.fillna(0).apply(lambda x: 1 if x > 0 else 0)
-    # CourtCase["IsAppealedcase"] = CourtCase.AppealedCourtCaseID.fillna(0).apply(lambda x: 1 if x > 0 else 0)
+    CourtCase["HasProsecutionCase"] = CourtCase.ProsecutionCaseInstanceID.fillna(0).apply(lambda x: 1 if x > 0 else 0)    
     CourtCase["ColorID"] = CourtCase.ColorID.fillna(-1)
     CourtCase["InstanceLevelID"] = CourtCase.InstanceLevelID.fillna(-1)
+    CourtCase["ExtraOrdinaryProcedureID"] = CourtCase.ExtraOrdinaryProcedureID.fillna(-1)
+    CourtCase["SpecialCaseID"] = CourtCase.SpecialCaseID.fillna(-1)
     CourtCase["SubCategoryID"] = CourtCase.SubCategoryID.fillna(-1)
     CourtCase["CasePriorityID"] = CourtCase.CasePriorityID.fillna(-1)
     CourtCase["IsDetentionCase"] = CourtCase.IsDetentionCase.fillna(0)
@@ -124,30 +131,51 @@ def data_preprocessing(data):
     CourtCase["GenderBasedViolence"] = CourtCase.GenderBasedViolence.fillna(0)
     CourtCase["InitiatedFromAbunzi"] = CourtCase.InitiatedFromAbunzi.fillna(0)
     CourtCase["SolvedFromAbunzi"] = CourtCase.SolvedFromAbunzi.fillna(0)
-    #CourtCase["HasDetails"] = CourtCase.HasDetails.fillna(0)
+    CourtCase["AppealedCourtCaseInstanceID"] = CourtCase.AppealedCourtCaseInstanceID.fillna(-1)
+    CourtCase["HasAppealCase"] = CourtCase.AppealedCourtCaseInstanceID.apply(lambda x: 0 if x == -1 else 1)
+    
+    CourtCase["ProsecutionCaseInstanceID"] = CourtCase.ProsecutionCaseInstanceID.fillna(-1)
+    CourtCase["HasProsecutionCase"] = CourtCase.ProsecutionCaseInstanceID.apply(lambda x: 0 if x == -1 else 1)
+        
+    CourtCase["ReceiptDocumentID"] = CourtCase.ReceiptDocumentID.fillna(-1)
+    CourtCase["HasReceiptDocument"] = CourtCase.ReceiptDocumentID.apply(lambda x: 0 if x == -1 else 1)
+    
+    CourtCase["PreviousCourtCaseInstanceID"] = CourtCase.PreviousCourtCaseInstanceID.fillna(-1)
+    CourtCase["HasPreviousCase"] = CourtCase.PreviousCourtCaseInstanceID.apply(lambda x: 0 if x == -1 else 1)    
     CourtCase["IsExempted"] = CourtCase.IsExempted.fillna(0)
     CourtCase["AttachedDate"] = CourtCase.AttachedDate.fillna(0)
-    CourtCase.drop(columns=["HasPassedCaseNumberAllocated", "CaseCode", "MinorVersion", "MajorVersion", "CourtID", "CourtCaseID"
-                       , "ReceiptDocumentID",  "ProsecutionCaseID", "WFActionID"
-                       , "NotRegisteredCaseCode", "WFStateID", "UpdatedUserID", "OwnerUserID", "PublicOwnerUserId", 'CreatedUserID'
-                       ,'AppealedCourtCaseID', "CountOfJudgmentPages"
-                      ], inplace=True)    
+    
+    CourtCase["CountryID"] = CourtCase.CountryID.fillna(-1)
+    CourtCase["ProvinceID"] = CourtCase.ProvinceID.fillna(-1)
+    CourtCase["DistrictID"] = CourtCase.DistrictID.fillna(-1)
+    CourtCase["ParentGroupID"] = CourtCase.DistrictID.fillna(-1)
+    CourtCase["avg_size"] = CourtCase.avg_size.fillna(0)
+    CourtCase["total_size"] = CourtCase.total_size.fillna(0)
+    CourtCase.drop(columns=["ExternalCases", "ReceiptNumber", "StatutoryInstruments", "ChainID"], inplace=True)    
   
-    drop_col = ['DecisionPronouncementDate', 'DecisionPronouncementDateYearID',
-       'ExecutionCaseApprovedUserID', 'PaymentBankID', 'LitigationCaseID',
-       'CaseRejectionID', 'ExtraOrdinaryProcedureID', 'PreviousCourtCaseID',
-       'SpecialCaseID']
-    CourtCase = CourtCase.drop(columns=drop_col)
-    #print(CourtCase.isna().any())
-    CourtCase.dropna(inplace=True)
+    CourtCase['FillingFee'] = pd.to_numeric(CourtCase['FillingFee'])
     print(CourtCase.shape)
     
-    assert len(CourtCase) > 0, 'No data left after preprocessing'    
+    assert len(CourtCase) > 0, 'No data left after preprocessing'
+    
+#     CourtCase = CourtCase[CourtCase["DecisionDuration"] < 30]
+    
+#     train_data = CourtCase[CourtCase['DecisionEndDate'] <= 43294]
+#     test_data = CourtCase[(CourtCase['DecisionEndDate'] > 43294)]
+    
+#     train_data["DecisionDuration"] = train_data["DecisionDuration"].apply(lambda x: 60 if x>= 60 else x)
+    
+#     train_X = train_data.drop(columns=["DecisionDuration", "DecisionEndDate"])
+#     train_Y = train_data["DecisionDuration"]
+    
+#     test_X = test_data.drop(columns=["DecisionDuration", "DecisionEndDate"])
+#     test_Y = test_data["DecisionDuration"]
 
-    X = CourtCase.drop(columns=["DecisionDuration"])
+    X = CourtCase.drop(columns=["DecisionDuration", "DecisionEndDate"])
     Y = CourtCase["DecisionDuration"]
-
     return X, Y
+#     print(train_X.shape, test_X.shape)
+#     return train_X, train_Y, test_X, test_Y
 
 def get_connection(path):
     cwd = os.getcwd()
@@ -175,15 +203,28 @@ def parse_args_train(*argument_array):
 def init(PROPERTIES_PATH, LOAD_FROM_DISK):
 	
 	
-	boost_params = {'n_estimators': 200,
- 'min_samples_split': 40,
- 'min_samples_leaf': 4,
- 'max_features': 'sqrt',
- 'max_depth': 20,
- 'learning_rate': 0.05}
- 
-	boost = GradientBoostingRegressor(**boost_params)
+#	boost_params = {'n_estimators': 200,
+# 'min_samples_split': 40,
+# 'min_samples_leaf': 4,
+# 'max_features': 'sqrt',
+# 'max_depth': 20,
+# 'learning_rate': 0.05}
+# 
+#	boost = GradientBoostingRegressor(**boost_params)
 
+	boost = LGBMRegressor(
+ learning_rate =0.05,
+ n_estimators=1127,
+ max_depth=-1,
+ min_child_weight=0,
+ num_leaves = 68,
+ min_child_samples = 5,
+ objective= 'regression',
+ subsample_for_bin = 1000,
+ min_split_gain = 0,
+ feature_fraction = 0.5, 
+ nthread=-1
+)
 	train_data = load_all_data(get_connection(PROPERTIES_PATH), TABLE_LIST, is_train=True, load_from_disk = LOAD_FROM_DISK)
 	train_data = data_preprocessing(train_data)
 	train_X, train_Y = train_data
